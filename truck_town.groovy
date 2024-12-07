6bc1851dd5af38f9cf1dd73629f5b66220759798
Daniel Schulz <faulolio@gmail.com>
2024-12-07 19:03:47 +0100

    build: mac, web

--- truck_town.groovy
+++ truck_town.groovy
@@ -45,6 +45,28 @@ pipeline {
                             
                             zip(zipFile: "${BUILD_NAME} - ${BUILD_PLATFORM}.zip", dir: "${BUILD_DIR}/${BUILD_PLATFORM}", archive: true, overwrite: true)
                         }
+                        stage('Build: MacOS') {
+							env.BUILD_PLATFORM = "macOS"
+							
+							fileOperations([folderCreateOperation("${BUILD_DIR}/${BUILD_PLATFORM}")])
+						
+                            catchError(buildResult: 'UNSTABLE', stageResult: 'UNSTABLE') {
+                                callShell "godot --headless --verbose --quit --export-debug \"${BUILD_PLATFORM}\" \"${BUILD_DIR}/${BUILD_PLATFORM}/${BUILD_NAME}.exe\""
+                            }
+                            
+                            zip(zipFile: "${BUILD_NAME} - ${BUILD_PLATFORM}.zip", dir: "${BUILD_DIR}/${BUILD_PLATFORM}", archive: true, overwrite: true)
+                        }
+                        stage('Build: WebGL') {
+							env.BUILD_PLATFORM = "Web"
+							
+							fileOperations([folderCreateOperation("${BUILD_DIR}/${BUILD_PLATFORM}")])
+						
+                            catchError(buildResult: 'UNSTABLE', stageResult: 'UNSTABLE') {
+                                callShell "godot --headless --verbose --quit --export-debug \"${BUILD_PLATFORM}\" \"${BUILD_DIR}/${BUILD_PLATFORM}/${BUILD_NAME}.exe\""
+                            }
+                            
+                            zip(zipFile: "${BUILD_NAME} - ${BUILD_PLATFORM}.zip", dir: "${BUILD_DIR}/${BUILD_PLATFORM}", archive: true, overwrite: true)
+                        }
                     }
                 }
             }
