19d1959c0925001bfa950dd40398765113eeed9d
Daniel Schulz <faulolio@gmail.com>
2024-12-07 18:49:56 +0100

    build: Linux

--- truck_town.groovy
+++ truck_town.groovy
@@ -7,7 +7,7 @@ pipeline {
         GODOT_VERSION = '4.3'
         PROJECT_LOCATION = '3d/truck_town'
         BUILD_NAME = 'Truck Town'
-		BUILD_DIR = 'build/windows'
+		BUILD_DIR = 'build'
     }
 	options {
 		disableConcurrentBuilds()
@@ -17,21 +17,33 @@ pipeline {
         stage('Setup') {
             steps {
                 script {
-                    dir(env.PROJECT_LOCATION) {
-						fileOperations([folderCreateOperation(env.BUILD_DIR)])
-							
+                    dir(env.PROJECT_LOCATION) {							
                         stage('Import Assets') {
                             catchError(buildResult: 'UNSTABLE', stageResult: 'UNSTABLE') {
                                 callShell "godot --headless --verbose --quit --editor --import"
                             }
                         }
                         stage('Build: Windows') {
+							env.BUILD_PLATFORM = "Windows Desktop"
+							
+							fileOperations([folderCreateOperation("${BUILD_DIR}/${BUILD_PLATFORM}")])
 						
                             catchError(buildResult: 'UNSTABLE', stageResult: 'UNSTABLE') {
-                                callShell "godot --headless --verbose --quit --export-debug \"Windows Desktop\" \"${BUILD_DIR}/${BUILD_NAME}.exe\""
+                                callShell "godot --headless --verbose --quit --export-debug \"${BUILD_PLATFORM}\" \"${BUILD_DIR}/${BUILD_PLATFORM}/${BUILD_NAME}.exe\""
                             }
                             
-                            zip(zipFile: "${BUILD_NAME}.zip", dir: "${BUILD_DIR}", archive: true)
+                            zip(zipFile: "${BUILD_NAME} - ${BUILD_PLATFORM}.zip", dir: "${BUILD_DIR}/${BUILD_PLATFORM}", archive: true)
+                        }
+                        stage('Build: Linux') {
+							env.BUILD_PLATFORM = "Linux"
+							
+							fileOperations([folderCreateOperation("${BUILD_DIR}/${BUILD_PLATFORM}")])
+						
+                            catchError(buildResult: 'UNSTABLE', stageResult: 'UNSTABLE') {
+                                callShell "godot --headless --verbose --quit --export-debug \"${BUILD_PLATFORM}\" \"${BUILD_DIR}/${BUILD_PLATFORM}/${BUILD_NAME}.exe\""
+                            }
+                            
+                            zip(zipFile: "${BUILD_NAME} - ${BUILD_PLATFORM}.zip", dir: "${BUILD_DIR}/${BUILD_PLATFORM}", archive: true)
                         }
                     }
                 }
