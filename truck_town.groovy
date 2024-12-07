a948fd2490cb464857b12999a7a4d137e9bbb014
Daniel Schulz <faulolio@gmail.com>
2024-12-07 18:31:54 +0100

    build: truck_town

---
+++ truck_town.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent {
+        label 'windows'
+    }
+    environment {
+        GODOT_IMAGE = 'barichello/godot-ci'
+        GODOT_VERSION = '4.3'
+        PROJECT_LOCATION = '3d/truck_town'
+        BUILD_NAME = 'Truck Town'
+		BUILD_DIR = 'build/windows'
+    }
+    stages {
+        stage('Setup') {
+            steps {
+                script {
+                    dir(env.PROJECT_LOCATION) {
+                        stage('Import Assets') {
+                            catchError(buildResult: 'UNSTABLE', stageResult: 'UNSTABLE') {
+                                callShell "godot --headless --verbose --quit --editor --import"
+                            }
+                        }
+                        stage('Build: Windows') {
+                            catchError(buildResult: 'UNSTABLE', stageResult: 'UNSTABLE') {
+                                callShell "godot --headless --verbose --quit --export-debug \"Windows Desktop\" \"${BUILD_DIR}/${BUILD_NAME}.exe\""
+                            }
+                            
+                            zip(zipFile: "${BUILD_NAME}.zip", dir: "${BUILD_DIR}", archive: true)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
